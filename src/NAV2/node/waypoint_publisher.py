b5153b9b34a58498783c03d73d286fbe96a48e12
nano5555 <sanyovippro@gmail.com>
2025-10-04 20:39:25 +0000

    test keyboard input

--- src/NAV2/node/waypoint_publisher.py
+++ src/NAV2/node/waypoint_publisher.py
@@ -6,50 +6,34 @@ from geometry_msgs.msg import PoseStamped
 import sys
 import termios
 import tty
+import select
+import threading
 
-def get_key(timeout=0.1):
-    fd = sys.stdin.fileno()
-    old_settings = termios.tcgetattr(fd)
-    try:
-        tty.setraw(fd)
-        rlist, _, _ = select.select([sys.stdin], [], [], timeout)
-        if rlist:
-            key = sys.stdin.read(1)
-        else:
-            key = None
-    finally:
-        termios.tcsetattr(fd, termios.TCSADRAIN, old_settings)
-    return key
-
-class WaypointPublisher(Node):
-
+class KeyboardNode(Node):
     def __init__(self):
-        super().__init__('waypoint_publisher')
-        self.publisher_ = self.create_publisher(PoseStamped, 'goal_pose', 10)        
-        self.x = 7.0
-        self.y = 8.0
+        super().__init__('keyboard_node')
+        self.get_logger().info('Keyboard node started. Press keys to see them. Press Ctrl+C to exit.')
+        self.settings = termios.tcgetattr(sys.stdin)
+        self.running = True
+        self.thread = threading.Thread(target=self.keyboard_loop,daemon=True)
+        self.thread.start()
 
-    def check_move(self):
-        key = get_key()
+    def keyboard_loop(self):
+        fd = sys.stdin.fileno()
+        while self.running:
+            rlist, _, _ = select.select([sys.stdin], [], [], 0.0)
+            key = sys.stdin.read(1) if rlist else None
+            self.get_logger().info(f'Key pressed: {repr(key)}') if key else None
 
-        if (key != None):
-            pose = PoseStamped()
-            pose.header.frame_id = "map"   # change to your TF frame
-            pose.header.stamp = self.get_clock().now().to_msg()
-            pose.pose.position.x = self.x
-            pose.pose.position.y = self.y
-            pose.pose.orientation.w = 1.0  # facing forward, no rotation
-
-            self.publisher_.publish(pose)
-
-        return
-
-
+    def destroy_node(self):
+        self.running = False 
+        termios.tcsetattr(fd, termios.TCSADRAIN, self.settings)
+        super().destroy_node()
 
 def main(args=None):
     rclpy.init(args=args)
 
-    node = WaypointPublisher()
+    node = KeyboardNode()
 
     rclpy.spin(node)
 
