c7d3ca1b56f17fcbf171aba19ee2fa233ec97730
Elliot Kantor <elliotjkantor@gmail.com>
2026-02-19 12:50:12 -0500

    Bring up nav2, ignore rvis

--- src/vipnav/launch/launch_sim.launch.py
+++ src/vipnav/launch/launch_sim.launch.py
@@ -11,96 +11,147 @@ from launch.substitutions import LaunchConfiguration
 from launch_ros.actions import Node
 
 
-
 def generate_launch_description():
-
 
     # Include the robot_state_publisher launch file, provided by our own package. Force sim time to be enabled
     # !!! MAKE SURE YOU SET THE PACKAGE NAME CORRECTLY !!!
 
-    package_name='vipnav' #<--- CHANGE ME
+    package_name = "vipnav"  # <--- CHANGE ME
 
     rsp = IncludeLaunchDescription(
-                PythonLaunchDescriptionSource([os.path.join(
-                    get_package_share_directory(package_name),'launch','rsp.launch.py'
-                )]), launch_arguments={'use_sim_time': 'true', 'use_ros2_control': 'false'}.items()
+        PythonLaunchDescriptionSource(
+            [
+                os.path.join(
+                    get_package_share_directory(package_name), "launch", "rsp.launch.py"
+                )
+            ]
+        ),
+        launch_arguments={"use_sim_time": "true", "use_ros2_control": "false"}.items(),
     )
 
     keyboard = IncludeLaunchDescription(
-                PythonLaunchDescriptionSource([os.path.join(
-                    get_package_share_directory(package_name),'launch','keyboard.launch.py'
-                )]))
+        PythonLaunchDescriptionSource(
+            [
+                os.path.join(
+                    get_package_share_directory(package_name),
+                    "launch",
+                    "keyboard.launch.py",
+                )
+            ]
+        )
+    )
 
     rviz2 = IncludeLaunchDescription(
-                PythonLaunchDescriptionSource([os.path.join(
-                    get_package_share_directory(package_name),'launch','rviz2.launch.py'
-                )]))
+        PythonLaunchDescriptionSource(
+            [
+                os.path.join(
+                    get_package_share_directory(package_name),
+                    "launch",
+                    "rviz2.launch.py",
+                )
+            ]
+        )
+    )
 
-    twist_mux_params = os.path.join(get_package_share_directory(package_name),'config','twist_mux.yaml')
+    twist_mux_params = os.path.join(
+        get_package_share_directory(package_name), "config", "twist_mux.yaml"
+    )
     twist_mux = Node(
-            package="twist_mux",
-            executable="twist_mux",
-            parameters=[twist_mux_params, {'use_sim_time': True}],
-            remappings=[('/cmd_vel_out','/diff_cont/cmd_vel_unstamped')]
+        package="twist_mux",
+        executable="twist_mux",
+        parameters=[twist_mux_params, {"use_sim_time": True}],
+        remappings=[("/cmd_vel_out", "/diff_cont/cmd_vel_unstamped")],
     )
 
     slam = IncludeLaunchDescription(
-                PythonLaunchDescriptionSource([os.path.join(
-                    get_package_share_directory(package_name),'launch','slam.launch.py'
-                )]), launch_arguments={'slam_params_file' : os.path.join(
-                    get_package_share_directory(package_name),'config', 'mapper_params_online_async_yaml')}.items()
+        PythonLaunchDescriptionSource(
+            [
+                os.path.join(
+                    get_package_share_directory(package_name),
+                    "launch",
+                    "slam.launch.py",
+                )
+            ]
+        ),
+        launch_arguments={
+            "slam_params_file": os.path.join(
+                get_package_share_directory(package_name),
+                "config",
+                "mapper_params_online_async_yaml",
+            )
+        }.items(),
+    )
+
+    # Include NAV2 bringup
+    nav2_bringup_launch = IncludeLaunchDescription(
+        PythonLaunchDescriptionSource(
+            [
+                os.path.join(
+                    get_package_share_directory("nav2_bringup"),
+                    "launch",
+                    "bringup_launch.py",
+                )
+            ]
+        ),
+        launch_arguments={"slam": "False", "use_sim_time": "true"}.items(),
     )
 
     default_world = os.path.join(
-        get_package_share_directory(package_name),
-        'worlds',
-        'obstacles.world'
-        )    
-    
-    world = LaunchConfiguration('world')
+        get_package_share_directory(package_name), "worlds", "obstacles.world"
+    )
+
+    world = LaunchConfiguration("world")
 
     world_arg = DeclareLaunchArgument(
-        'world',
-        default_value=default_world,
-        description='World to load'
-        )
+        "world", default_value=default_world, description="World to load"
+    )
 
     # Include the Gazebo launch file, provided by the ros_gz_sim package
     gazebo = IncludeLaunchDescription(
-                PythonLaunchDescriptionSource([os.path.join(
-                    get_package_share_directory('ros_gz_sim'), 'launch', 'gz_sim.launch.py')]),
-                    launch_arguments={'gz_args': ['-r -v4 ', world], 'on_exit_shutdown': 'true'}.items()
-             )
+        PythonLaunchDescriptionSource(
+            [
+                os.path.join(
+                    get_package_share_directory("ros_gz_sim"),
+                    "launch",
+                    "gz_sim.launch.py",
+                )
+            ]
+        ),
+        launch_arguments={
+            "gz_args": ["-r -v4 ", world],
+            "on_exit_shutdown": "true",
+        }.items(),
+    )
 
     # Run the spawner node from the ros_gz_sim package. The entity name doesn't really matter if you only have a single robot.
-    spawn_entity = Node(package='ros_gz_sim', executable='create',
-                        arguments=['-topic', 'robot_description',
-                                   '-name', 'my_bot',
-                                   '-z', '0.1'],
-                        output='screen')
+    spawn_entity = Node(
+        package="ros_gz_sim",
+        executable="create",
+        arguments=["-topic", "robot_description", "-name", "my_bot", "-z", "0.1"],
+        output="screen",
+    )
 
-
-    bridge_params = os.path.join(get_package_share_directory(package_name),'config','gz_bridge.yaml')
+    bridge_params = os.path.join(
+        get_package_share_directory(package_name), "config", "gz_bridge.yaml"
+    )
     ros_gz_bridge = Node(
         package="ros_gz_bridge",
         executable="parameter_bridge",
         arguments=[
-            '--ros-args',
-            '-p',
-            f'config_file:={bridge_params}',
-        ]
+            "--ros-args",
+            "-p",
+            f"config_file:={bridge_params}",
+        ],
     )
 
     ros_gz_image_bridge = Node(
         package="ros_gz_image",
         executable="image_bridge",
-        arguments=["/camera/image_raw"]
+        arguments=["/camera/image_raw"],
     )
 
-
-
     # Code for delaying a node (I haven't tested how effective it is)
-    # 
+    #
     # First add the below lines to imports
     # from launch.actions import RegisterEventHandler
     # from launch.event_handlers import OnProcessExit
@@ -115,18 +166,19 @@ def generate_launch_description():
     #
     # Replace the diff_drive_spawner in the final return with delayed_diff_drive_spawner
 
-
-
     # Launch them all!
-    return LaunchDescription([
-        rsp,
-        keyboard,
-        twist_mux,
-        slam,
-        rviz2,
-        world_arg,
-        gazebo,
-        spawn_entity,
-        ros_gz_bridge,
-        ros_gz_image_bridge
-    ])
+    return LaunchDescription(
+        [
+            rsp,
+            keyboard,
+            twist_mux,
+            slam,
+            nav2_bringup_launch,
+            # rviz2,
+            world_arg,
+            gazebo,
+            spawn_entity,
+            ros_gz_bridge,
+            ros_gz_image_bridge,
+        ]
+    )
